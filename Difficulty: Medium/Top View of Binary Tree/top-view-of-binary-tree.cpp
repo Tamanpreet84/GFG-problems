a852d43628c8f114eba374c4424abcfb53c75792
Taman Preet Singh <149267683+Tamanpreet84@users.noreply.github.com>
2026-07-16 10:47:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -14,42 +14,38 @@ class Node {
 */
 
 class Solution {
-  public:
+public:
     vector<int> topView(Node *root) {
-        map<int,int> mp;
-        queue<pair<Node*,int>> q;
-        q.push({root,0});
-        
-        int min_level = 0;
-        int max_level = 0;
-        
-        while(!q.empty()){
-            
-            Node* Current = q.front().first;
-            int axes = q.front().second;
-            q.pop();
-            
-            min_level = min(min_level,axes);
-            max_level = max(max_level,axes);
-            
-            if(Current->left!=NULL){
-                q.push({Current->left,axes-1});
-            }
-            
-            if(Current->right!=NULL){
-                q.push({Current->right,axes+1});   
-            }
-            
-            if(mp.find(axes)==mp.end()){
-                mp[axes]=Current->data;
-            }
-        }
-        
+
         vector<int> ans;
-        for(int i=min_level;i<=max_level;i++){
-            ans.push_back(mp[i]);
+
+        if (root == NULL)
+            return ans;
+
+        map<int, int> mp;
+        queue<pair<Node*, int>> q;
+
+        q.push({root, 0});
+
+        while (!q.empty()) {
+
+            Node* temp = q.front().first;
+            int hd = q.front().second;
+            q.pop();
+
+            if (mp.find(hd) == mp.end())
+                mp[hd] = temp->data;
+
+            if (temp->left)
+                q.push({temp->left, hd - 1});
+
+            if (temp->right)
+                q.push({temp->right, hd + 1});
         }
-        
+
+        for (auto x : mp)
+            ans.push_back(x.second);
+
         return ans;
     }
 };
